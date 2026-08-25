f8836c6b691ba8ee9da158ff43772945f1ec3cdb
seiya0302 <32158119+seiya0302@users.noreply.github.com>
2026-08-25 21:37:10 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,1 @@
-const CACHE='temple-ops-v1';
-const ASSETS=['./','./index.html','./manifest.webmanifest'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>{
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
-});
+const C='temple-v3';const A=['./','./index.html','./manifest.webmanifest','./temple_expedition_original.wav'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
